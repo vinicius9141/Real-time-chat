13db8c98533d6f8844465caa9f03e8be1afa109f
Vinicius Henrique <vinicius9141@gmail.com>
2023-08-01 15:22:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// Initialize Firebase com as configurações do seu projeto
+const firebaseConfig = {
+     apiKey: "AIzaSyACaYPAwSRj8Yl4QZ-DXavyZsRMablJ1R4",
+    authDomain: "realtimechat-6e34d.firebaseapp.com",
+    projectId: "realtimechat-6e34d",
+    storageBucket: "realtimechat-6e34d.appspot.com",
+    messagingSenderId: "296544290773",
+    appId: "1:296544290773:web:e3fbd18022861ed6ce5f5c"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Referência ao banco de dados do Firebase
+const database = firebase.database();
+
+// Referências aos elementos da página
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+const chatMessages = document.getElementById('chat-messages');
+
+// Função para enviar mensagem
+function sendMessage() {
+    const message = messageInput.value.trim();
+    if (message !== '') {
+        const timestamp = Date.now();
+        database.ref('messages/' + timestamp).set({
+            message: message
+        });
+        messageInput.value = '';
+    }
+}
+
+// Evento ao clicar no botão enviar
+sendButton.addEventListener('click', sendMessage);
+
+// Evento ao pressionar Enter no campo de input
+messageInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+// Monitorando alterações no banco de dados do Firebase
+database.ref('messages').on('child_added', function (snapshot) {
+    const messageData = snapshot.val();
+    const messageElement = document.createElement('div');
+    messageElement.textContent = messageData.message;
+    chatMessages.appendChild(messageElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
